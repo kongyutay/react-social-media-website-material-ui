fc88e66107995070a7251f278390f022841dbd7f
Tay Kong Yu <kongyutay@gmail.com>
2024-05-28 20:08:43 +0800

    Finished Navbar and Sidebar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Mail, Notifications, Pets } from "@mui/icons-material";
-import { AppBar, Avatar, Badge, Box, InputBase, Toolbar, Typography, styled } from "@mui/material";
-import React from "react";
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, Toolbar, Typography, styled } from "@mui/material";
+import React, { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
     display:"flex",
@@ -14,12 +14,26 @@ const Search = styled("div")(({ theme }) => ({
     width: "40%"
 }));
 const Icons = styled(Box)(({ theme }) => ({
-    display: "flex",
+    display: "none",
     gap: "20px",
-    alignItems: "center"
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+        display: "flex"
+    }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    gap: "10px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+        display: "none"
+    }
 }));
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <AppBar position="sticky">
             <StyledToolbar>
@@ -33,9 +47,31 @@ const Navbar = () => {
                     <Badge badgeContent={2} color="error">
                         <Notifications />
                     </Badge>
-                    <Avatar sx={{width:30, height:30}} src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+                    <Avatar sx={{width:30, height:30}} src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" onClick={(e)=>setOpen(true)}/>
                 </Icons>
+                <UserBox onClick={(e)=>{setOpen(true)}}>
+                    <Avatar sx={{width:30, height:30}} src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+                    <Typography variant="span">John</Typography>
+                </UserBox>
             </StyledToolbar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={(e)=>setOpen(false)}
+                anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+            >
+                <MenuItem >Profile</MenuItem>
+                <MenuItem >My account</MenuItem>
+                <MenuItem >Logout</MenuItem>
+            </Menu>
         </AppBar>
     );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,78 @@
-import { Box } from "@mui/material";
+import { AccountBox, Home, ModeNight, Settings } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
     return (
-        <Box bgcolor="skyblue" flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}}>Sidebar</Box>
+        <Box flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}}>
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="Homepage" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="Pages" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="Groups" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="Marketplace" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="Friends" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <Settings />
+                        </ListItemIcon>
+                        <ListItemText primary="Settings" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <AccountBox />
+                        </ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <ModeNight />
+                        </ListItemIcon>
+                        <Switch />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
     )
 }
 
