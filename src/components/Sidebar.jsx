42756ca3a192e862ee0412aababa8a04166156b9
Tay Kong Yu <kongyutay@gmail.com>
2024-06-01 16:51:06 +0800

    Finished whole project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { AccountBox, Home, ModeNight, Settings } from "@mui/icons-material";
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
 import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({mode, setMode}) => {
     return (
         <Box flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}}>
             <Box position="fixed">
@@ -69,7 +69,7 @@ const Sidebar = () => {
                             <ListItemIcon>
                                 <ModeNight />
                             </ListItemIcon>
-                            <Switch />
+                            <Switch onChange={e=>setMode(mode === "light" ? "dark" : "light")}/>
                         </ListItemButton>
                     </ListItem>
                 </List>
