61e33c5ba48050a45afb7036ec42d57f7992c491
Tay Kong Yu <kongyutay@gmail.com>
2024-06-01 13:14:48 +0800

    Finished rightbar component

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,10 +1,10 @@
-import { Avatar, AvatarGroup, Box, ImageList, Typography } from "@mui/material";
+import { Avatar, AvatarGroup, Box, Divider, ImageList, ImageListItem, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import React from "react";
 
 const Rightbar = () => {
     return (
         <Box flex={2} p={2} sx={{display: {xs: "none", sm: "block"}}}>
-            <Box position="fixed">
+            <Box position="fixed" width={300}>
                 <Typography variant="h6" fontWeight={100}>OnlineFriends</Typography>
                 <AvatarGroup max={6}>
                     <Avatar alt="Remy Sharp" src="https://images.pexels.com/photos/8993561/pexels-photo-8993561.jpeg?auto=compress&cs=tinysrgb&w=600" />
@@ -15,11 +15,86 @@ const Rightbar = () => {
                     <Avatar alt="Trevor Henderson" src="https://images.pexels.com/photos/4946515/pexels-photo-4946515.jpeg?auto=compress&cs=tinysrgb&w=600" />
                     <Avatar alt="Trevor Henderson" src="https://images.pexels.com/photos/4420634/pexels-photo-4420634.jpeg?auto=compress&cs=tinysrgb&w=600" />
                 </AvatarGroup>
-                <Typography variant="h6" fontWeight={100}>Latest Photos Until 54:16</Typography>
-                <ImageList>
-
+                <Typography variant="h6" fontWeight={100} mt={2} mb={2}>Latest Photos</Typography>
+                <ImageList cols={3} rowHeight={100} gap={5}>
+                    <ImageListItem>
+                        <img src="https://mui.com/static/images/image-list/breakfast.jpg" alt="" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://mui.com/static/images/image-list/breakfast.jpg" alt="" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://mui.com/static/images/image-list/breakfast.jpg" alt="" />
+                    </ImageListItem>
                 </ImageList>
-
+                <Typography variant="h6" fontWeight={100} mt={2}>Latest Conversations</Typography>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary="Brunch this weekend?"
+                        secondary={
+                            <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                            </React.Fragment>
+                        }
+                        />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary="Summer BBQ"
+                        secondary={
+                            <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                to Scott, Alex, Jennifer
+                            </Typography>
+                            {" — Wish I could come, but I'm out of town this…"}
+                            </React.Fragment>
+                        }
+                        />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary="Oui Oui"
+                        secondary={
+                            <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Sandra Adams
+                            </Typography>
+                            {' — Do you have Paris recommendations? Have you ever…'}
+                            </React.Fragment>
+                        }
+                        />
+                    </ListItem>
+                </List>
             </Box>
         </Box>
     )
